485ee66a05faf92366ddb205e4fb555e7348f39b
Octave <octave.mesquita@epitech.eu>
2022-10-19 13:15:16 +0200

    [FIX]: Fix date formatting

--- src/app/components/date-picker/date-picker.component.ts
+++ src/app/components/date-picker/date-picker.component.ts
@@ -1,7 +1,8 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {TUI_DATE_FORMAT, TUI_DATE_SEPARATOR} from "@taiga-ui/cdk";
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {TUI_DATE_FORMAT, TUI_DATE_SEPARATOR, TuiDay, TuiTime} from "@taiga-ui/cdk";
 import {tuiCreateTimePeriods} from "@taiga-ui/kit";
 import {ApiService} from "../../services/api.service";
+import {TuiAlertService, TuiNotification} from "@taiga-ui/core";
 
 @Component({
   selector: 'app-date-picker',
@@ -13,25 +14,42 @@ import {ApiService} from "../../services/api.service";
     {provide: TUI_DATE_SEPARATOR, useValue: `-`},
   ]
 })
-export class DatePickerComponent implements OnInit {
-  date: any;
-  time: any;
+export class DatePickerComponent {
+  date = new TuiDay(2022, 9, 19);
+  time = new TuiTime(12, 0);
 
-  availableTimes = tuiCreateTimePeriods(10, 20, [0, 30]);
+  readonly timeSlots = tuiCreateTimePeriods(10, 20, [0, 30]);
 
   constructor(
-    private apiService: ApiService
-  ) { }
-
-  ngOnInit(): void {
-  }
+    @Inject(TuiAlertService)
+    private readonly alertService: TuiAlertService,
+    private apiService: ApiService,
+  ) {}
 
   async checkIfAvailable() {
-    const formatedDate = (`${this.date}`).split('.').join('-');
-    // alert(formatedDate)
+    const formatedDate = this.date.getFormattedDay('YMD', '-')
     const data = await this.apiService.checkDateTime(`${formatedDate}T${this.time}:00Z`);
     const { available } = data;
 
-    alert(available);
+    if (available) {
+      this.displayNotification(
+        `This slot is available !`,
+        `The ressource is available on ${formatedDate} at ${this.time}. You can start the reservation process`,
+        TuiNotification.Success
+      );
+    } else {
+      this.displayNotification(
+        `This slot is not available !`,
+        `The ressource is not available on ${formatedDate} at ${this.time}`,
+        TuiNotification.Error
+      )
+    }
+  }
+
+  displayNotification(title: string, body: string, type: TuiNotification) {
+    this.alertService.open(body, {
+      label: title,
+      status: type
+    }).subscribe();
   }
 }
